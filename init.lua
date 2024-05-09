5e6bc0b52d2d4283ba19a0b8060c4e7e89a3f2d9
Georgiy <metallerok@gmail.com>
2024-05-09 17:41:54 +0300

    plugins organisation

--- init.lua
+++ init.lua
@@ -25,29 +25,7 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    {'mhartington/oceanic-next', name="oceanic-next", priority = 1000},
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    {'f-person/git-blame.nvim', name="git-blame"},
-    {'tanvirtin/vgit.nvim', requires = {'nvim-lua/plenary.nvim'}},
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
-    },
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 require('vgit').setup()
 
 local builtin = require("telescope.builtin")
