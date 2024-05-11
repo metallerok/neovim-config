35f80b5f977fa90a91feda248090a5430e984512
Georgiy <metallerok@gmail.com>
2024-05-11 12:04:15 +0300

    completions colors

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -14,7 +14,59 @@ return {
         config = function ()
             local cmp = require('cmp')
             require("luasnip.loaders.from_vscode").lazy_load()
+
+            -- gray
+            vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg='NONE', strikethrough=true, fg='#808080' })
+            -- blue
+            vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg='NONE', fg='#569CD6' })
+            vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link='CmpIntemAbbrMatch' })
+            -- light blue
+            vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg='NONE', fg='#9CDCFE' })
+            vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link='CmpItemKindVariable' })
+            vim.api.nvim_set_hl(0, 'CmpItemKindText', { link='CmpItemKindVariable' })
+            -- pink
+            vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg='NONE', fg='#C586C0' })
+            vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link='CmpItemKindFunction' })
+            -- front
+            vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg='NONE', fg='#D4D4D4' })
+            vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link='CmpItemKindKeyword' })
+            vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link='CmpItemKindKeyword' })
+
+            local cmp_kinds = {
+              Text = '  ',
+              Method = '  ',
+              Function = '  ',
+              Constructor = '  ',
+              Field = '  ',
+              Variable = '  ',
+              Class = '  ',
+              Interface = '  ',
+              Module = '  ',
+              Property = '  ',
+              Unit = '  ',
+              Value = '  ',
+              Enum = '  ',
+              Keyword = '  ',
+              Snippet = '  ',
+              Color = '  ',
+              File = '  ',
+              Reference = '  ',
+              Folder = '  ',
+              EnumMember = '  ',
+              Constant = '  ',
+              Struct = '  ',
+              Event = '  ',
+              Operator = '  ',
+              TypeParameter = '  ',
+            }
+
             cmp.setup({
+                formatting = {
+                    format = function(_, vim_item)
+                        vim_item.kind = (cmp_kinds[vim_item.kind] or '') .. vim_item.kind
+                        return vim_item
+                    end,
+                },
                 snippet = {
                   -- REQUIRED - you must specify a snippet engine
                   expand = function(args)
@@ -22,8 +74,8 @@ return {
                   end,
                 },
                 window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
+                    -- completion = cmp.config.window.bordered(),
+                    -- documentation = cmp.config.window.bordered(),
                 },
                 mapping = {
                   ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
