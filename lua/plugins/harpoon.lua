18c72b40328decf0da92b0f3763918a54fa2c3e1
Georgiy <metallerok@gmail.com>
2024-05-14 21:49:33 +0300

    Harpoon telescope removing items

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,10 +6,20 @@ return {
         local harpoon = require("harpoon").setup()
         local conf = require("telescope.config").values
 
-        local function toggle_telescope(harpoon_files)
+
+       local function toggle_telescope(harpoon_files)
             local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
+            for i, item in ipairs(harpoon_files.items) do
                 table.insert(file_paths, item.value)
+            end
+
+            local make_finder = function()
+                local paths = {}
+                for _, item in ipairs(harpoon_files.items) do
+                  table.insert(paths, item.value)
+                end
+
+                return require('telescope.finders').new_table { results = paths }
             end
 
             require("telescope.pickers").new({}, {
@@ -19,19 +29,19 @@ return {
                 }),
                 previewer = conf.file_previewer({}),
                 sorter = conf.generic_sorter({}),
-                -- attach_mappings = function(prompt_buffer_number, map)
-                --     map("i", "<c-d>", function()
-                --       local state = require("telescope.actions.state")
-                --       local selected_entry = state.get_selected_entry()
-                --       local current_picker = state.get_current_picker(prompt_buffer_number)
-                --
-                --       -- This is the line you need to remove the entry
-                --       harpoon:list():remove(selected_entry)
-                --       current_picker:refresh()
-                --     end)
-                --
-                --     return true
-                -- end,
+                attach_mappings = function(prompt_buffer_number, map)
+                    map("n", "<c-d>", function()
+                      local state = require("telescope.actions.state")
+                      local selected_entry = state.get_selected_entry()
+                      local current_picker = state.get_current_picker(prompt_buffer_number)
+
+                      -- This is the line you need to remove the entry
+                      harpoon:list():remove(selected_entry)
+                      current_picker:refresh(make_finder())
+                    end)
+
+                    return true
+                end,
             }):find()
         end
 
