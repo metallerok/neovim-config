d888deeea928dabf3ea0625c4f62d381719db69c
Georgiy <metallerok@gmail.com>
2024-05-27 12:27:58 +0300

    catpputcchin colorscheme

---
+++ lua/plugins/oceanic_colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'mhartington/oceanic-next',
+        name="oceanic-next",
+        priority = 1000,
+        config = function()
+        -- vim.cmd.colorscheme "OceanicNext"
+        end
+    },
+}
